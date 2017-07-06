f79b4e42974f8d3c47951899245c605095070039
SuzeShardlow <suze.shardlow1@gmail.com>
2017-07-07 00:20:03 +0100

    deleted duplicate JS file

--- js/app.js
+++ js/app.js
@@ -1,132 +1,16 @@
-var p1 = p1 || {};
-var boardList;
-var chosenGrid;
-var clickCount = 0;
-var clickedBox;
-let gridNumber = Math.floor((Math.random() * 10));
-var newText;
-var userSequence = [];
+let p1 = {};
 
-const solved = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', ''];
+let activeArrayIndex;
+let boardList;
+let chosenGrid;
+let clickCount = 0;
+let clickedBox;
+let clock;
+const gridNumber = Math.floor((Math.random() * 10));
+let gridSet;
+let time;
 
-// add another for letters game
-// click button - selectedGame - pass into all the functions to reference that array when doing the comparison
-// create LIs after selecting the game to be played
-// hide menu when the game starts
-// shake board if they don't select modal
-
-
-$(() => {
-  p1.getGrid = function() {
-    chosenGrid = grids[gridNumber];
-    console.log(gridNumber);
-    boardList = $('ul.gameboard');
-  }; // end of getGrid
-  p1.getGrid();
-
-
-  p1.populateBoard = function populateBoard() {
-    $.each(chosenGrid, function(i) {
-      const li = $('<li/>')
-      .addClass('gridbox')
-      .addClass('font-effect-emboss')
-      .attr('id', [i])
-      .appendTo(boardList)
-      .text(chosenGrid[i]);
-      if ((i + 1) === chosenGrid[i]) {
-        li.addClass('correct');
-      }
-    }); // end of .each
-  }; // end of populateBoard
-  p1.populateBoard();
-
-
-  p1.timer = function() {
-    var time = 60;
-    onTimer();
-    function onTimer() {
-      $('#timerbutton').off('click');
-      $('#timerbutton').text(time);
-      time--;
-      if (time < 0) {
-        $('#modalmask').show();
-        $('#modaltext').text('Time\'s up!  Want to play again?');
-        $('#modaldialogue').show();
-      } else {
-        setTimeout(onTimer, 1000);
-      }
-    }
-  };
-
-// on first click start the timer
-// moments.js
-// do a bit of maths
-
-
-  p1.getClick = function getClick() {
-    let boxes = $('.gridbox');
-    boxes.on('click', (e) => {
-      clickedBox = parseInt(($(e.currentTarget).attr('id')));
-      let blankBoxId = $('.gridbox:empty').attr('id');
-      if (legalMoves[blankBoxId].includes(clickedBox)) {
-        clickCount++;
-        $('#clicks').text('Number of moves played: ' + clickCount);
-        p1.moveTile = function moveTile() {
-          console.log('Number of moves played: ',clickCount);
-          $('.gridbox:empty').addClass('font-effect-emboss');
-          $('.gridbox:empty').text(e.currentTarget.innerText);
-          $(e.currentTarget).removeClass('font-effect-emboss');
-          $(e.currentTarget).text(null);
-        };
-
-        p1.moveTile();
-        let gameSequence = [];
-        for (let n of $('.gridbox')) {
-          gameSequence.push(n.innerText);
-        }
-        let userWon = true;
-        for (let b = 0; b < solved.length; b++) {
-          let currentTile = $('#' + b);
-          if (solved[b] !== gameSequence[b]) {
-            userWon = false;
-            currentTile.removeClass('correct');
-          } else {
-            if (currentTile.text() !== '') {
-              if (! currentTile.hasClass('correct')) {
-                currentTile.addClass('correct');
-              }
-            }
-          }
-        }
-
-        if (userWon) {
-          $('#modalmask').show();
-          $('#modaltext').text('You won in ' + clickCount + ' moves!  Want to play again?');
-          $('#modaldialogue').show();
-        }
-      } else {
-        $(e.currentTarget).addClass('animated shake');
-        setTimeout(function () {
-          $(e.currentTarget).removeClass('animated shake');
-        }, 1000);
-      }
-    }); // end of click event
-  }; // end of getClick
-}); // the last brace and bracket of the DOM loading function
-
-p1.setup = function setup() {
-  p1.getClick();
-  $('#timerbutton').on('click', p1.timer);
-  $('#modalyes').on('click', function() {
-    location.reload();
-  });
-  $('#modalno').on('click', function() {
-    $('#modaldialogue').hide();
-    $('#modalmask').hide();
-  });
-}; // end of p1.setup
-
-const grids = [
+const numberGrids = [
   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, '', 15],
   [ 3, 6,  7, 10,  5,  9, 15,  '',  4, 13, 12,  2,  1,  8, 14, 11],
   [ 9, 5, 12,  8,  6,  3,  4, 11, 15,  7, 14, 10,  '',  2,  1, 13],
@@ -139,6 +23,40 @@ const grids = [
   [ 9, 1,  4,  7,  6, 14,  3,  '',  8, 10,  2,  5, 11, 13, 15, 12],
   [14, 6,  '', 12,  7, 13,  2,  3, 11, 15,  1,  8,  4,  5,  9, 10]
 ];
+
+const letterGrids = [
+  ['C', 'B', 'A', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '', 'O']
+];
+
+const letterNumbers = {
+  0: ['A', '1'],
+  1: ['B', '2'],
+  2: ['C', '3'],
+  3: ['D', '4'],
+  4: ['E', '5'],
+  5: ['F', '6'],
+  6: ['G', '7'],
+  7: ['H', '8'],
+  8: ['I', '9'],
+  9: ['J', '10'],
+  10: ['K', '11'],
+  11: ['L', '12'],
+  12: ['M', '13'],
+  13: ['N', '14'],
+  14: ['O', '15'],
+  15: ['', '']
+};
+
 const legalMoves = [
   [1, 4],
   [0, 2, 5],
@@ -157,4 +75,145 @@ const legalMoves = [
   [10, 13, 15],
   [11, 14]
 ];
-$(p1.setup.bind(p1)); // function to sort any THIS issues
+
+p1.gameChoice = function gameChoice() {
+  let choice = $('.mode');
+  choice.on('click', (e) => {
+    p1.stopCountdown();
+    clickCount = 0;
+    $('#clicks').text('Number of moves played: ' + clickCount);
+    choice.removeClass('active');
+    $(e.currentTarget).addClass('active');
+    gridSet = ($(e.currentTarget).attr('id'));
+    gridSet === 'number' ? activeArrayIndex = 1 : activeArrayIndex = 0;
+    p1.getGrid();
+  });
+};
+
+p1.getGrid = function getGrid() {
+  gridSet === 'number' ? chosenGrid = numberGrids[gridNumber]:chosenGrid = letterGrids[gridNumber];
+  boardList = $('ul.gameboard');
+  p1.populateBoard();
+};
+
+
+p1.populateBoard = function populateBoard() {
+  boardList.empty();
+  $.each(chosenGrid, function(i) {
+    const li = $('<li/>')
+    .addClass('gridbox')
+    .addClass('font-effect-emboss')
+    .appendTo(boardList)
+    .attr('id', i)
+    .text(chosenGrid[i]);
+
+    if (activeArrayIndex === 1) {
+      if ((i + 1) === chosenGrid[i]){
+        console.log(li.id);
+        console.log(chosenGrid[i]);
+        li.addClass('correct');
+      }
+    } else {
+      if(letterNumbers[i][activeArrayIndex] === chosenGrid[i]) {
+        li.addClass('correct');
+      }
+    }
+  });
+  p1.getClick();
+};
+
+
+p1.countDown = function countDown() {
+  time = 120;
+  onCountDown();
+  function onCountDown() {
+    $('#countdown').text('Seconds remaining: ' + time);
+    time--;
+    if (time < 0) {
+      $('#modalmask').show();
+      $('#modaltext').html('Time\'s up!  Want to play again?');
+      $('#modaldialogue').show();
+    } else {
+      clock = setTimeout(onCountDown, 1000);
+    }
+  }
+};
+
+
+p1.getClick = function getClick() {
+  let boxes = $('.gridbox');
+  boxes.on('click', (e) => {
+    clickedBox = parseInt(($(e.currentTarget).attr('id')));
+    let blankBoxId = $('.gridbox:empty').attr('id');
+    if (legalMoves[blankBoxId].includes(clickedBox)) {
+      clickCount++;
+      $('#clicks').text('Number of moves played: ' + clickCount);
+
+      if (clickCount === 1) {
+        p1.countDown();
+      }
+      p1.moveTile = function moveTile() {
+        $('.gridbox:empty').addClass('font-effect-emboss');
+        $('.gridbox:empty').text(e.currentTarget.innerText);
+        $(e.currentTarget).removeClass('font-effect-emboss');
+        $(e.currentTarget).text(null);
+      };
+      p1.moveTile();
+      const gameSequence = [];
+      for (let n of $('.gridbox')) {
+        gameSequence.push(n.innerText);
+      }
+      let userWon = true;
+      for (let b = 0; b < 16; b++) {
+        const currentTile = $('#' + b);
+        if (letterNumbers[b][activeArrayIndex] !== gameSequence[b]) {
+          userWon = false;
+          currentTile.removeClass('correct');
+        } else {
+          if (currentTile.text() !== '') {
+            if (!currentTile.hasClass('correct')) {
+              currentTile.addClass('correct');
+            }
+          }
+        }
+      }
+
+      if (userWon) {
+        p1.stopCountdown();
+        $('#modalmask').show();
+        $('#modaltext').text('You won in ' + clickCount + ' moves and ' + time + ' seconds!  Want to play again?');
+        $('#modaldialogue').show();
+      }
+    } else {
+      $(e.currentTarget).addClass('animated shake');
+      setTimeout(function () {
+        $(e.currentTarget).removeClass('animated shake');
+      }, 1000);
+    }
+  });
+};
+
+
+p1.stopCountdown = function stopCountdown() {
+  if (clock) {
+    clearTimeout(clock);
+  }
+  $('#countdown').text('COUNTDOWN...');
+};
+
+
+p1.setup = function setup() {
+  p1.gameChoice();
+  $('#modalyes').on('click', function() {
+    location.reload();
+  });
+  $('#modalno').on('click', function() {
+    $('#modaldialogue').hide();
+    $('#modalmask').hide();
+  });
+};
+
+
+$(() => {
+  p1.setup();
+});
